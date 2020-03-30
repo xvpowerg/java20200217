0cd30fd587bb1075b8cbeaf10b93383485d2a7f0
xvpowerg <c911010083@gmail.com>
2020-03-30 20:16:32 +0800

    add ArrayList

---
+++ ch18_20200330/test_collection/Ch18_3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch18_20200330.test_collection;
+import java.util.List;
+import java.util.ArrayList;
+public class Ch18_3 {
+
+    public static void main(String[] args) {
+	//List  是一組介面
+	//ArrayList
+	List arrayList = new ArrayList();
+	arrayList.add(20);
+	arrayList.add(10);
+	arrayList.add(31);
+	arrayList.add(57);
+	arrayList.add(83);
+	//LinkedList
+	
+    }
+    
+}
