9e14580bd1bb6fced6fa0e086afcc4721b5ef36c
xvpowerg <c911010083@gmail.com>
2020-03-30 19:20:55 +0800

    Add files via upload

---
+++ ch18_20200330/test_enum/Ch18_1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch18_20200330.test_enum;
+
+/**
+ *
+ * @author xvpow
+ */
+public class Ch18_1 {
+    
+    public static void main(String[] args) {
+         Fruit f1 = FruitFactor.createFruit(3);
+	System.out.println(f1.getName()+":"+ f1.getPrice());
+
+    }
+    
+}
